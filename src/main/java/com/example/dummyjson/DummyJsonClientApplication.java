0eed2fb7c1773adc9666aab83b2591352bd42f29
Pedro Henrique <pedrolobato1305@gmail.com>
2025-04-03 11:01:10 -0300

    IBM-O1 Adding documentation to the main class

--- src/main/java/com/example/dummyjson/DummyJsonClientApplication.java
+++ src/main/java/com/example/dummyjson/DummyJsonClientApplication.java
@@ -3,9 +3,23 @@ package com.example.dummyjson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for the DummyJsonClientApplication.
+ *
+ * This class initializes and runs the Spring Boot application. It serves as the main
+ * configuration and launching point for the application by invoking the SpringApplication.run
+ * method with the class reference and program arguments.
+ */
 @SpringBootApplication
 public class DummyJsonClientApplication {
 
+    /**
+     * The entry point of the application.
+     *
+     * This method launches the Spring Boot application by invoking the SpringApplication.run method.
+     *
+     * @param args command-line arguments passed to the application
+     */
     public static void main(String[] args) {
         SpringApplication.run(DummyJsonClientApplication.class, args);
     }
